5d74f808f4ce77765d00685966bb7abcacdc9e56
jonfreibr <jfreivald@brmedical.com>
2024-10-25 06:58:02 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -8,14 +8,17 @@ rem     python-3.11.5-amd64.exe     The Python source file
 rem     requirements.txt            Used by Python to install required libraries
 rem     unattend.xml                Required to run a "hands-off" install of Python.
 rem
-rem echo Installing Python 3.11.5
+h:
+cd "\_BRMCApps\QR Code Generator"
+echo Installing Python 3.11.5
 python-3.11.5-amd64.exe /passive
-rem echo upgrading pip
-%LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip
-rem echo Adding package requients
-%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r requirements.txt
-rem echo Copying files
-copy /y "QR Generator.lnk" %USERPROFILE%\Desktop
-rem echo Done with installation
+echo upgrading pip
+%LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip -q
+echo Adding package requirements
+%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r requirements.txt -q
+echo Copying shortcut to Desktop
+copy /y "QR Generator.lnk" %USERPROFILE%\Desktop > nul 2>&1
+echo Done with installation, Running QR Generator
+"%USERPROFILE%\Desktop\QR Generator.lnk"
 rem pause
 rem exit
